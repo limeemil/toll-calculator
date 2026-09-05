db0b46bcf43fc9bedcc25f53c4a4db4366e3e218
limeemil <andreas.lonnermark@gmail.com>
2026-09-05 19:18:21 +0200

    Moved enum months

--- C#/TollCalculator.cs
+++ C#/TollCalculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Globalization;
 using TollFeeCalculator;
+using static TollFeeCalculator.TollFeeSchedule;
 
 public class TollCalculator
 {
@@ -100,19 +101,5 @@ public class TollCalculator
         Military = 5
     }
 
-    private enum Months
-    {
-        January = 1,
-        February = 2,
-        March = 3,
-        April = 4,
-        May = 5,
-        June = 6,
-        July = 7,
-        August = 8,
-        September = 9,
-        October = 10,
-        November = 11,
-        December = 12
-    }
+    
 }
--- C#/TollFeeSchedule.cs
+++ C#/TollFeeSchedule.cs
@@ -14,12 +14,28 @@ namespace TollFeeCalculator
                 return true;
             }
             // Check for public holidays (example: January 1st, December 25th)
-            if ((date.Month == 1 && date.Day == 1) || (date.Month == 5 && date.Day == 1) || 
-                (date.Month == 12 && (date.Day == 24 || date.Day == 25 || date.Day == 26 || date.Day == 31)))
+            if ((date.Month == (int)Months.January && date.Day == 1) || (date.Month == (int)Months.May && date.Day == 1) || 
+                (date.Month == (int)Months.June && date.Day == 6) ||
+                (date.Month == (int)Months.December && (date.Day == 24 || date.Day == 25 || date.Day == 26 || date.Day == 31)))
             {
                 return true;
             }
             return false;
+        
+        public enum Months
+        {
+            January = 1,
+            February = 2,
+            March = 3,
+            April = 4,
+            May = 5,
+            June = 6,
+            July = 7,
+            August = 8,
+            September = 9,
+            October = 10,
+            November = 11,
+            December = 12
         }
     }
 }
